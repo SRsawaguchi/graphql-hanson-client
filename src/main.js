6019acb9d54da90d81f3ce3edfebed9f03ea192a
Naoya Sawaguchi <naoyasawaguchi978@gmail.com>
2020-12-15 18:52:00 +0900

    Installed vue-qraphql

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueApollo from 'vue-apollo'
+import { newApolloClient } from './graphql/Client'
 
 Vue.config.productionTip = false
+Vue.use(VueApollo)
+
+const apolloClient = newApolloClient('http://localhost:3020/query')
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 new Vue({
   render: h => h(App),
+  apolloProvider,
 }).$mount('#app')
